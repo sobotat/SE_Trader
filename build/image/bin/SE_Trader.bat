07cc8cca2026a72e244e16bde3c16a5d0354d0cf
Tomáš Sobota <tomas.sobota@gmail.com>
2022-03-18 21:30:57 +0100

    1.0

---
+++ build/image/bin/SE_Trader.bat
@@ -0,0 +1,7 @@
+@echo off
+set DIR="%~dp0"
+set JAVA_EXEC="%DIR:"=%\java"
+
+
+
+pushd %DIR% & %JAVA_EXEC% %CDS_JVM_OPTS%  -p "%~dp0/../app" -m com.setrader.se_trader/com.setrader.se_trader.Main  %* & popd
